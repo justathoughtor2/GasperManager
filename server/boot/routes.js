9c91496410bd1f3deea66ab9613623546847d8de
justathoughtor2 <thoughthaven@gmail.com>
2015-01-12 23:39:18 -0500

    Upload dialog

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,8 +1,29 @@
 module.exports = function(app) {
   var router = app.loopback.Router();
 
-  router.get('/', function(req, res) {
-    res.send('Hello world!');
+  router.get('*', function(req, res) {
+    //res.send('Hello world!');
+
+    var formidable = require('formidable'),
+      util = require('util');
+
+    // Parse a file upload
+    if(req.url == '/upload' && req.method.toLowerCase() == 'post') {
+      var form = new formidable.IncomingForm();
+      form.parse(req, function(err, field, files) {
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.write('received upload:\n\n');
+        res.end(util.inspect({fields: fields, files: files}));
+      });
+    }
+
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end(
+      '<form action="/upload" enctype="multipart/form-data" method="post">' +
+      '<input type="text" name="title" /><br />' +
+      '<input type="submit" value="Upload" />' +
+      '</form>'
+    );
   });
 
   app.use(router);
